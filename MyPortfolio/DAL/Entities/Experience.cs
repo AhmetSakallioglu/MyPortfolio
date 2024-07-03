3aff7a5bcce5d51354da387ee4608a324e3d3064
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2024-07-03 17:25:13 +0300

    Add-Migration

--- MyPortfolio/DAL/Entities/Experience.cs
+++ MyPortfolio/DAL/Entities/Experience.cs
@@ -2,7 +2,7 @@
 {
     public class Experience
     {
-        public int ExperinceId { get; set; }
+        public int ExperienceId { get; set; }
         public string Head { get; set; }
         public string Title { get; set; }
         public string Date { get; set; }
