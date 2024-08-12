c8625427ee2fa58645a11f785e3d791f0e058a30
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2024-08-12 15:47:57 +0300

    Create Experience

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
 
 namespace MyPortfolio.Controllers
 {
@@ -10,6 +11,19 @@ namespace MyPortfolio.Controllers
 		{
 			var values = context.Experiences.ToList();
 			return View(values);
+		}
+
+		[HttpGet]
+		public IActionResult CreateExperience()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult CreateExperience(Experience experience)
+		{
+			context.Experiences.Add(experience);
+			context.SaveChanges();
+			return RedirectToAction(nameof(ExperienceList));
 		}
 	}
 }
