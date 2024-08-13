cfa9bac7be600292038657e1331ff46b6eb75fb2
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2024-08-13 13:24:31 +0300

    Statistics

---
+++ MyPortfolio/Controllers/StatisticController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+	public class StatisticController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Index()
+		{
+			ViewBag.YetenekSayisi = context.Skills.Count();
+			ViewBag.MesajSayisi = context.Messages.Count();
+			ViewBag.OkunmayanMesaj = context.Messages.Where(x => x.IsRead == false).Count();
+			ViewBag.OkunanMesaj = context.Messages.Where(x => x.IsRead == true).Count();
+
+			return View();
+		}
+	}
+}
