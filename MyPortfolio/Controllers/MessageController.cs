6ba068237de931acecfdb4f1ec847f50dbcce837
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2024-08-13 13:50:46 +0300

    Inbox Page - Change Message Status

---
+++ MyPortfolio/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+	public class MessageController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Inbox()
+		{
+			var values = context.Messages.ToList();
+			return View(values);
+		}
+
+		public IActionResult ChangeIsReadToTrue(int id)
+		{
+			var value = context.Messages.Find(id);
+			value.IsRead = true;
+			context.SaveChanges();
+			return RedirectToAction(nameof(Inbox));
+		}
+
+		public IActionResult ChangeIsReadToFalse(int id)
+		{
+			var value = context.Messages.Find(id);
+			value.IsRead = false;
+			context.SaveChanges();
+			return RedirectToAction(nameof(Inbox));
+		}
+
+	}
+}
